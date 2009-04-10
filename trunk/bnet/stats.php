a6794c4ba13dcc4f217c1d11adb3542a9bd40867
HarpyWar <HarpyWar@28d7a00c-24fe-11de-96e2-9591d3992c9b>
2009-04-10 09:26:56 +0000

    mysql select results optimize

--- trunk/bnet/stats.php
+++ trunk/bnet/stats.php
@@ -36,23 +36,42 @@ if ( $searchPlayer )
 {
 	if ( strlen($searchPlayer) >= $min_search_symbols )
 	{
-		$condition = 'LIKE "%' . mysql_escape_string($searchPlayer) . '%"';
+		$condition = 'LIKE "%' . strtolower(mysql_escape_string($searchPlayer)) . '%"';
 		
+	/*
+		// long search #1
 		$result = MYSQL_QUERY("SELECT uid, SEXP_1_last_game_result, SEXP_1_rating, SEXP_1_rank, SEXP_0_wins, SEXP_0_losses,  SEXP_0_disconnects, SEXP_1_wins, SEXP_1_losses, SEXP_0_draws,SEXP_1_draws FROM $table_record 
 		WHERE uid IN ( SELECT uid FROM $table_bnet WHERE acct_username $condition ) 
 		ORDER BY SEXP_1_rating DESC;");
 
+		// long search #2
+		$result = MYSQL_QUERY("SELECT {$table_record}.uid,SEXP_1_last_game_result, SEXP_1_rating, SEXP_1_rank, SEXP_0_wins, SEXP_0_losses,  SEXP_0_disconnects, SEXP_1_wins, SEXP_1_losses, SEXP_0_draws,SEXP_1_draws, 
+		{$table_bnet}.acct_username, {$table_bnet}.acct_lastlogin_ip, {$table_bnet}.acct_lastlogin_time
+		FROM $table_record  LEFT JOIN $table_bnet
+		ON {$table_record}.uid = {$table_bnet}.uid
+		WHERE username $condition ORDER BY SEXP_1_rating DESC;");
+	*/	
+	
+		// fast search
+		$result = MYSQL_QUERY("SELECT record.uid, record.SEXP_1_last_game_result, record.SEXP_1_rating, record.SEXP_1_rank, record.SEXP_0_wins, record.SEXP_0_losses, record.SEXP_0_disconnects, record.SEXP_1_wins, record.SEXP_1_losses, record.SEXP_0_draws,record.SEXP_1_draws,
+		{$table_bnet}.acct_username, {$table_bnet}.acct_lastlogin_ip, {$table_bnet}.acct_lastlogin_time
+		FROM $table_bnet
+		LEFT JOIN $table_record
+		ON {$table_bnet}.uid = {$table_record}.uid
+		WHERE username $condition and {$table_record}.SEXP_1_rank > 0;");
+
+	
 		$players_count = mysql_num_rows($result);
 	}
 	else
 	{
-		echo sprintf("Please, input %s or more symbols", $min_search_symbols);
+		echo sprintf("<h3 align=center>Please, input %s or more symbols</h3>", $min_search_symbols);
 	}
 }
 // иначе выбрать всех игроков
 else
 {	
-	$result = MYSQL_QUERY("SELECT (select count(uid) from $table_record) as count, uid, SEXP_1_last_game_result, SEXP_1_rating, SEXP_1_rank, SEXP_0_wins, SEXP_0_losses,  SEXP_0_disconnects, SEXP_1_wins, SEXP_1_losses, SEXP_0_draws,SEXP_1_draws FROM $table_record 
+	$result = MYSQL_QUERY("SELECT (select count(*) from $table_record) as count, uid, SEXP_1_last_game_result, SEXP_1_rating, SEXP_1_rank, SEXP_0_wins, SEXP_0_losses,  SEXP_0_disconnects, SEXP_1_wins, SEXP_1_losses, SEXP_0_draws,SEXP_1_draws FROM $table_record 
 	ORDER BY SEXP_1_rating DESC LIMIT $lim_first,$players_on_page;");
 
 	$players_count = mysql_result($result,0,"count");
@@ -66,9 +85,14 @@ $number = @MYSQL_NUM_ROWS($result); #всего выбрано
 for ($i=0;$i<$number;$i++)
 {
 	$p_uid=mysql_result($result,$i,"uid"); #player unicue id
+	
 	$p_SEXP_1_last_game_result=mysql_result($result,$i,"SEXP_1_last_game_result"); 
 	$p_SEXP_1_rating=mysql_result($result,$i,"SEXP_1_rating"); 
 	$p_SEXP_1_rank=mysql_result($result,$i,"SEXP_1_rank"); 
+	
+	#if (!$p_SEXP_1_rank) // display only SEXP players
+	#	continue;
+
 	$p_SEXP_0_wins=mysql_result($result,$i,"SEXP_0_wins"); 
 	$p_SEXP_0_losses=mysql_result($result,$i,"SEXP_0_losses"); 
 	$p_SEXP_0_draws=mysql_result($result,$i,"SEXP_0_draws"); 
@@ -76,11 +100,20 @@ for ($i=0;$i<$number;$i++)
 	$p_SEXP_1_losses=mysql_result($result,$i,"SEXP_1_losses"); 
 	$p_SEXP_1_draws=mysql_result($result,$i,"SEXP_1_draws"); 
 
-	$result2 = MYSQL_QUERY("SELECT acct_userid,acct_username, acct_lastlogin_ip,acct_lastlogin_time FROM $table_bnet WHERE uid='$p_uid' LIMIT 1");
-		$p_acct_userid=mysql_result($result2,0,"acct_userid");
-		$p_acct_username=mysql_result($result2,0,"acct_username");
-		$p_acct_lastlogin_time=mysql_result($result2,0,"acct_lastlogin_time");
-		$p_acct_lastlogin_ip=mysql_result($result2,0,"acct_lastlogin_ip");
+	if ($searchPlayer)
+	{
+		$result2 = $result;
+		$j = $i;
+	}
+	else
+	{
+		$result2 = MYSQL_QUERY("SELECT acct_username, acct_lastlogin_ip,acct_lastlogin_time FROM $table_bnet WHERE uid='$p_uid' LIMIT 1");
+		$j = 0;
+	}
+	
+	$p_acct_username=mysql_result($result2,$j,"acct_username");
+	$p_acct_lastlogin_time=mysql_result($result2,$j,"acct_lastlogin_time");
+	$p_acct_lastlogin_ip=mysql_result($result2,$j,"acct_lastlogin_ip");
 
 	$allgames=$p_SEXP_0_wins+$p_SEXP_0_losses+$p_SEXP_1_wins+$p_SEXP_1_losses;
 	$alldraws=$p_SEXP_0_draws+$p_SEXP_1_draws;
@@ -102,7 +135,8 @@ MYSQL_CLOSE();
 
 	echo "<tr><td colspan=10><small>";
 	
-	echo pageNavigator($current_page, $players_on_page, $players_count, $trim_count, "index.php?game=SEXP&type=1&");
+	if (!$searchPlayer)
+		echo pageNavigator($current_page, $players_on_page, $players_count, $trim_count, "index.php?game=SEXP&type=1&");
 	
 
 	echo "</small></td></tr>";
